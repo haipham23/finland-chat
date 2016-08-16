d0dba444be608a57edfd562c5edd59d88cef7c89
Hai Pham <haipham23@gmail.com>
2016-08-16 12:08:29 +0800

    add google login option, but temporarily disable it

--- server/user-accounts.method.js
+++ server/user-accounts.method.js
@@ -19,6 +19,60 @@ Meteor.methods({
   }
 });
 
+// Accounts.onCreateUser( ( options, user )=> {
+//
+//     user.profile  = getProfile( user );
+//
+//     function getProfile( user ) {
+//
+//         let service = user.services;
+//         let network = _.keys( service )[ 0 ];
+//         let data    = service[ network ];
+//         let profile = {};
+//
+//         if ( network === 'facebook' ) {
+//             profile.network = network;
+//             profile.id      = data.id;
+//             profile.email   = data.email;
+//             profile.name    = data.name;
+//             profile.locale  = data.locale;
+//             profile.gender  = data.gender;
+//             profile.picture = 'http://graph.facebook.com/' + data.id + '/picture?type=square';
+//         }
+//
+//         else if ( network === 'google' ) {
+//             profile.network = network;
+//             profile.id      = data.id;
+//             profile.email   = data.email;
+//             profile.name    = data.name;
+//             profile.locale  = data.locale;
+//             profile.picture = data.picture;
+//         }
+//
+//         else if ( network === 'twitter' ) {
+//             profile.network = network;
+//             profile.id      = data.id;
+//             profile.email   = data.email;
+//             profile.name    = data.screenName;
+//             profile.locale  = data.lang;
+//             profile.picture = data.profile_image_url_https;
+//         }
+//
+//         let value        = {};
+//         value[ network ] = profile;
+//         return value;
+//     }
+//
+//     Meteor.users.update( user._id, {
+//         $set: {
+//             profile: user.profile
+//         }
+//     } );
+//
+//     return user;
+//
+// } );
+
 Accounts.onCreateUser(function(options, user) {
   if (options.profile) {
     options.profile.picture = `http://graph.facebook.com/${user.services.facebook.id}/picture/?type=large`;
